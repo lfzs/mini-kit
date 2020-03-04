44078fec5abcf2828509e3197ed8cf647e0d24fb
jstsoso <jycomputer_417@163.com>
2020-03-04 19:01:49 +0800

    refactor: subscribe modal

--- src/util/subscribe-message-modal.js
+++ src/util/subscribe-message-modal.js
@@ -1,26 +1,28 @@
-import { fly } from '@util'
+import { fly, wxp } from '@util'
 
-export default function({ title = '', content = '', cancelText = '', confirmText = '确定', tmplIds = [], targetType = '', targetId = '' }) {
-  tmplIds = tmplIds.filter(Boolean)
-  if (!tmplIds.length) return
+export default async function({ content = '', confirmText = '确定', tmplIds = [], targetType = '', targetId = '' }) {
+  if (!tmplIds.length) return wxp.showToast({ title: content, icon: 'none' })
   wx.hideLoading()
 
   const sendSubscribeMessage = (res = {}) => {
     const acceptIds = Object.keys(res).filter(id => res[id] === 'accept')
-    if (acceptIds.length) {
-      fly.post('subscribe_records/batch', { targetable_type: targetType, targetable_id: targetId, template_ids: acceptIds })
-    } else {
-      wx.showToast({ title: '订阅失败，可在右上角设置中打开订阅', duration: 3000, icon: 'none' })
-    }
+    if (acceptIds.length) fly.post('subscribe_records/batch', { targetable_type: targetType, targetable_id: targetId, template_ids: acceptIds })
+  }
+
+  const { subscriptionsSetting: { mainSwitch, itemSettings = {} } } = await wxp.getSetting({ withSubscriptions: true })
+  if (!mainSwitch) return wxp.showToast({ title: content, icon: 'none' })
+
+  const onceIds = tmplIds.filter(id => !itemSettings[id]) // 订阅一次的 id
+  if (!onceIds.length) {
+    wx.requestSubscribeMessage({ success: res => sendSubscribeMessage(res) })
+    return wxp.showToast({ title: content, icon: 'none' })
   }
 
   return new Promise(resolve => {
     wx.showModal({
-      title,
       content,
-      showCancel: !!cancelText.length,
-      cancelText,
       confirmText,
+      showCancel: false,
       fail: resolve,
       success: ({ confirm }) => {
         if (!confirm) return resolve()
