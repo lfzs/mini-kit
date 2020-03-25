8c1ad9bda7f25cc879aa921e9b8deafe9dcbddd6
jstsoso <jycomputer_417@163.com>
2020-03-25 18:15:01 +0800

    feat: all list store

--- src/util/fetch-action.js
+++ src/util/fetch-action.js
@@ -1,13 +1,11 @@
 import { flow } from 'mobx'
 
-// 类方法装饰器
-export default function(target, name, descriptor) { //target 为构造函数
+export default function(target, name, descriptor) { // target 为类的原型对象
   const { value } = descriptor
   if (typeof value !== 'function') throw new Error(`${name} is not a function`)
 
   target.tryFetchData = function() { return this._state !== 'done' && this.fetchData() }
 
-  // this 指向实例 => 将 _state 暴露到实例上
   descriptor.value = flow(function* (...args) {
     this._state = 'pending'
     try {
