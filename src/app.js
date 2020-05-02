02cf40570b396042ed9f77c11cbfacd2d2075f99
jstsoso <jycomputer_417@163.com>
2020-05-02 11:36:02 +0800

    feat: upgrade package

--- src/app.js
+++ src/app.js
@@ -21,13 +21,7 @@ App({
   // 新版本提示
   checkUpdateManager() {
     const updateManager = wx.getUpdateManager()
-    updateManager.onUpdateReady(() => {
-      wx.showModal({
-        title: '提示',
-        content: '新版本已经准备好，是否重启更新应用？',
-        success: () => updateManager.applyUpdate(),
-      })
-    })
+    updateManager.onUpdateReady(() => updateManager.applyUpdate())
   },
 
 })
